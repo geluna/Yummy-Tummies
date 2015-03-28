afc00acd717a5e7a4219bc67ce727829da416d34
Giovanni Luna <geluna@valdosta.edu>
2015-03-28 16:28:05 -0400

    static page about works

--- Gemfile
+++ Gemfile
@@ -12,6 +12,12 @@ end
 
 gem 'high_voltage', '~> 2.2.1'
 
+gem 'better_errors'
+
+gem 'quiet_assets'
+
+gem 'rails_layout'
+
 gem 'cancancan', '~> 1.10'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
   #  delete 'logout' => :destroy
   #end
 
-
-
+    root 'store#index', as: 'store'
   #get 'sessions/create'
 
   #get 'sessions/destroy'
@@ -25,7 +24,7 @@ Rails.application.routes.draw do
 
   resources :menus
   
-  root 'store#index', as: 'store'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
