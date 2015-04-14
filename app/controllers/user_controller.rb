94c63351e36c0b315ea98533fac5e0ea86176e83
gakashkin <grisha.kashkin@gmail.com>
2015-04-13 22:58:12 -0400

    map updated with registered users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,11 +7,20 @@ class UserController < ApplicationController
   	else
     	@users = User.all
   	end
+
+ 
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+          
+end
   	
   end
 
   def show
-
+      @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      end
   end
   
   def update
