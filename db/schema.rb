d39cd5a4bea0a54c2be5acd3f1a2dbd1640ae18f
Giovanni Luna <geluna@valdosta.edu>
2015-04-22 15:01:20 -0400

    fixed student seed error and populated more students

--- db/schema.rb
+++ db/schema.rb
@@ -60,10 +60,7 @@ ActiveRecord::Schema.define(version: 20150416205015) do
     t.string   "school"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
-
-  add_index "students", ["user_id"], name: "index_students_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -54,45 +54,54 @@ Menu.delete_all
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Student.create!(
-      id:     1
-      fname: 'John',
-      lname: 'Jonson',
-      school: 'Valdosta Elementary School'
-      )
-Student.create!(
-      id:     2
-      fname: 'Mary',
-      lname: 'Hoppin',
-      school: 'Valdosta Elementary School'
-      )
-Student.create!(
-      id:     3
-      fname: 'Kevin',
-      lname: 'Edward',
-      school: 'Valdosta Elementary School'
-      )
-Student.create!(
-      id:     4
-      fname: 'Gary',
-      lname: 'Wood',
-      school: 'Albany Elementary School'
-      )
-Student.create!(
-      id:     5
-      fname: 'Roy',
-      lname: 'Bailey',
-      school: 'Albany Elementary School'
-      )
-Student.create!(
-      id:     6
-      fname: 'Lily',
-      lname: 'Robin',
-      school: 'Baytree Elementary School'
-      )
-Student.create!(
-      id:     7
-      fname: 'Louren',
-      lname: 'Clare',
-      school: 'Baytree Elementary School'
-      )
+Student.create!(id:     1, fname: 'Rick', lname: 'Grimes', school: 'Valdosta Elementary School')
+Student.create!(id:     2, fname: 'Mary', lname: 'Hoppin', school: 'Valdosta Elementary School')
+Student.create!(id:     3, fname: 'Kevin',lname: 'Edward', school: 'Valdosta Elementary School')
+Student.create!(id:     4, fname: 'Gary', lname: 'Wood',   school: 'Albany Elementary School')
+Student.create!(id:     5, fname: 'Roy',  lname: 'Bailey', school: 'Albany Elementary School')
+Student.create!(id:     6, fname: 'Lily', lname: 'Robin', school: 'Baytree Elementary School')
+Student.create!(id:     7, fname: 'Nyna', lname: 'Park',   school: 'Baytree Elementary School')
+Student.create!(id:     8, fname: 'Lauren', lname: 'Clare', school: 'Baytree Elementary School')
+Student.create!(id:     9, fname: 'Carl', lname: 'Stallone', school: 'Baytree Elementary School')
+Student.create!(id:     10, fname: 'Clark', lname: 'Kent', school: 'Baytree Elementary School')
+Student.create!(id:     11, fname: 'lucifer', lname: 'Satan', school: 'Baytree Elementary School')
+Student.create!(id:     12, fname: 'Giovanni', lname: 'Luna', school: 'Timberlane Elementary School')
+Student.create!(id:     13, fname: 'Kun', lname: 'Kim', school: 'Baytree Elementary School')
+Student.create!(id:     14, fname: 'Grigory', lname: 'Kashkin', school: 'Baytree Elementary School')
+Student.create!(id:     15, fname: 'Zhiguang', lname: 'Xu', school: 'Baytree Elementary School')
+Student.create!(id:     16, fname: 'Dave', lname: 'Gibson', school: 'Baytree Elementary School')
+Student.create!(id:     17, fname: 'Shet', lname: 'hitfan', school: 'Baytree Elementary School')
+Student.create!(id:     18, fname: 'Roger', lname: 'Willis', school: 'Baytree Elementary School')
+Student.create!(id:     19, fname: 'Susan', lname: 'Grim', school: 'Valdosta Elementary School')
+Student.create!(id:     20, fname: 'Lois', lname: 'Lane', school: 'Valdosta Elementary School')
+Student.create!(id:     21, fname: 'Jim', lname: 'Lee', school: 'Valdosta Elementary School')
+Student.create!(id:     22, fname: 'Jack', lname: 'Ryan', school: 'Valdosta Elementary School')
+Student.create!(id:     23, fname: 'Alexander', lname: 'Allen', school: 'Valdosta Elementary School')
+Student.create!(id:     24, fname: 'Alan', lname: 'Purvis', school: 'Valdosta Elementary School')
+Student.create!(id:     25, fname: 'Gary', lname: 'Pollock', school: 'Valdosta Elementary School')
+Student.create!(id:     26, fname: 'James', lname: 'Keen', school: 'Valdosta Elementary School')
+Student.create!(id:     27, fname: 'Juan', lname: 'Guerrero', school: 'Valdosta Elementary School')
+Student.create!(id:     28, fname: 'Muhammed', lname: 'Aquzi', school: 'Valdosta Elementary School')
+Student.create!(id:     29, fname: 'Xio', lname: 'Kim', school: 'Valdosta Elementary School')
+Student.create!(id:     30, fname: 'Ben', lname: 'Dover', school: 'Valdosta Elementary School')
+Student.create!(id:     31, fname: 'Dixie', lname: 'Normous',   school: 'Albany Elementary School')
+Student.create!(id:     32, fname: 'Jack', lname: 'Goff',   school: 'Albany Elementary School')
+Student.create!(id:     33, fname: 'Justin', lname: 'Sider',   school: 'Albany Elementary School')
+Student.create!(id:     34, fname: 'Thomas', lname: 'Fister',   school: 'Albany Elementary School')
+Student.create!(id:     35, fname: 'Justin', lname: 'Collins',   school: 'Albany Elementary School')
+Student.create!(id:     36, fname: 'Sarah', lname: 'Smith',   school: 'Albany Elementary School')
+Student.create!(id:     37, fname: 'Ron', lname: 'Jeremy',   school: 'Albany Elementary School')
+Student.create!(id:     38, fname: 'Christopher', lname: 'Holmes',   school: 'Albany Elementary School')
+Student.create!(id:     39, fname: 'Jesus', lname: 'Alvarez',   school: 'Albany Elementary School')
+Student.create!(id:     40, fname: 'Jose', lname: 'Portillo',   school: 'Timberlane Elementary School')
+Student.create!(id:     41, fname: 'Amir', lname: 'Jarjuri',   school: 'Timberlane Elementary School')
+Student.create!(id:     42, fname: 'Natalia', lname: 'Clifton',   school: 'Timberlane Elementary School')
+Student.create!(id:     43, fname: 'Donald', lname: 'Hatcher',   school: 'Timberlane Elementary School')
+Student.create!(id:     44, fname: 'Vanessa', lname: 'Clinton',   school: 'Timberlane Elementary School')
+Student.create!(id:     45, fname: 'Christina', lname: 'Fishbeck',   school: 'Timberlane Elementary School')
+Student.create!(id:     46, fname: 'Luis', lname: 'Mantilla',   school: 'Timberlane Elementary School')
+Student.create!(id:     47, fname: 'Ruben', lname: 'Rivera',   school: 'Timberlane Elementary School')
+Student.create!(id:     48, fname: 'Alana', lname: 'Meral',   school: 'Timberlane Elementary School')
+Student.create!(id:     49, fname: 'Kristina', lname: 'Ricci',   school: 'Timberlane Elementary School')
+Student.create!(id:     50, fname: 'Joaquin', lname: 'Grande',   school: 'Timberlane Elementary School')
+Student.create!(id:     51, fname: 'Oscar', lname: 'Rhymes',   school: 'Baytree Elementary School')
