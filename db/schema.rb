43b865e59d2367917d656a3031dc55f32d85716c
Giovanni Luna <geluna@valdosta.edu>
2015-04-22 16:28:24 -0400

    scaffold new Account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416205015) do
+ActiveRecord::Schema.define(version: 20150422202515) do
+
+  create_table "accounts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -52,6 +57,7 @@ ActiveRecord::Schema.define(version: 20150416205015) do
     t.string   "pay_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "total"
   end
 
   create_table "students", force: true do |t|
